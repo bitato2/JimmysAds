1969920eb660bf472bbc795c266bdbe9dad55704
bitato2 <61390457+bitato2@users.noreply.github.com>
2020-03-15 22:19:43 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var Token = process.env.BOT_TOKEN;
-var TimeBetween = 0.166667;
+var TimeBetween = 0.001;
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   const interval = setInterval(function(){
-    client.channels.get("682625693023993869").send(";bc");
+    var guildList = client.guilds.array();
+    try {
+        guildList.forEach(guild => guild.channels.get("name","jimmys-ads-botted").send(";bc"));
+    } catch (err) {
+        console.log("Could not send message to " + guild.name);
+    }
+    // client.channels.get("682625693023993869").send(";bc");
   },((TimeBetween*60)*60)*1000);
 })
 client.login(Token);
